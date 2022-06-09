11963a3bded3190f382edbe5ee0788b87418395e
RenanHMF <renanhmnf2@gmail.com>
2022-06-09 20:36:51 -0300

    fix: adicionado arquivo de script de criação de tabela

---
+++ FatecoMania/Scripts/script.sql
@@ -0,0 +1,11 @@
+-- ATENTION!!!!
+-- create table at database carddb;
+
+CREATE TABLE CARD (
+    ID INT PRIMARY KEY,
+    DESCRICAO VARCHAR(50),
+    NOME VARCHAR(100),
+    FORCA INT,
+    MAGIA INT,
+    FOGO INT
+);
